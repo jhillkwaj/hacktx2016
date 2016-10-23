28d6b99f3392459f47218e9a88b49ec646c5b38b
jhillkwaj <jhillkwaj@gmail.com>
2016-10-22 19:50:05 -0500

    Some movement working

---
+++ Maze/Assets/Move_Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move_Player : MonoBehaviour {
+
+    public GameObject head;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.DrawRay(this.transform.position, head.transform.forward);
+        this.transform.position += 5 * Input.GetAxis("Vertical") * Time.deltaTime * head.transform.forward;
+	}
+}
