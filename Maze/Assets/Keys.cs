04eab8a9b0674a4b50264a82cc003fc86d7acc4d
jhillkwaj <jhillkwaj@gmail.com>
2016-10-23 00:35:51 -0500

    work on keys

---
+++ Maze/Assets/Keys.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Keys : MonoBehaviour {
+
+    public GameObject[] keys;
+	
+	// Update is called once per frame
+	void Update () {
+	    for(int i = 0; i < keys.Length; i++)
+        {
+            if(keys[i]!=null)
+            {
+                return;
+            }
+        }
+        win();
+	}
+
+    void win()
+    {
+        Debug.Log("win");
+    }
+}
